863f5615e07bd4762860242aa82f36286b62bacd
Salma <salmaibrahim813@gmail.com>
2023-04-23 14:29:32 +0100

    styling changes

--- application/data_provider_service.py
+++ application/data_provider_service.py
@@ -10,7 +10,7 @@ class DataProviderService:
         host = 'localhost'
         port = 3306
         user = 'root'
-        password = 'password'
+        password = ''
         database = 'Voyago'
         self.conn = pymysql.connect(host=host, port=port, user=user, password=password, db=database)
         self.cursor = self.conn.cursor()
